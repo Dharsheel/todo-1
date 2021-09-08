e4d2b3b0d3646c3b943ee78443d4811a30d3b444
Darsheel <dharsheelahammed136@gmail.com>
2021-09-08 12:39:07 +0530

    new commit

---
+++ todo/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path 
+from . import views
+
+
+urlpatterns = [
+    path('',views.home),
+    path('login/',views.login),
+    path('register/',views.register),
+    path('todo/',views.todo),
+    path('save/',views.save),
+    path('delete/',views.delete),
+    path('logout/',views.logout),
+]
